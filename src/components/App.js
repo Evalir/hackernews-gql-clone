9b46b6fee00b69715ac49e0fc2effdd55e7e3c4f
Enrique Ortiz <enrique@cuyaya.com>
2019-09-16 23:41:07 -0400

    Add routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Header from './Header';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import '../styles/App.css';
 
 function App() {
-  return <CreateLink />;
+  return (
+    <>
+      <div className="center w85">
+        <BrowserRouter>
+          <Header />
+          <div className="ph3 pv1 background-gray">
+            <Switch>
+              <Route exact path="/" component={LinkList} />
+              <Route exact path="/create" component={CreateLink} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </div>
+    </>
+  );
 }
 
 export default App;
